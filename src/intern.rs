eb39cc4ff79c99d0baf3eea5091e24c46e5ff83a
torkleyy <torkleyy@gmail.com>
2018-03-28 16:18:32 +0200

    Add interned paths

--- src/intern.rs
+++ src/intern.rs
@@ -2,6 +2,12 @@
 #[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Interned(pub u64);
 
+impl Interned {
+    pub fn into_inner(self) -> u64 {
+        self.0
+    }
+}
+
 impl<'a> From<&'a [u8]> for Interned {
     fn from(bytes: &[u8]) -> Self {
         Interned(intern(bytes))
@@ -34,4 +40,48 @@ fn intern(mut s: &[u8]) -> u64 {
     }
 
     result
+}
+
+pub type InternedPath = [u64];
+
+#[repr(C)]
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct InternedPathBuf(Vec<u64>);
+
+impl<'a, T> From<&'a [T]> for InternedPathBuf
+where
+    &'a T: Into<Interned>,
+{
+    fn from(slice: &'a [T]) -> Self {
+        InternedPathBuf::from_iter(slice)
+    }
+}
+
+impl<'a> From<&'a str> for InternedPathBuf {
+    fn from(s: &str) -> Self {
+        InternedPathBuf::from_iter(s.split('/'))
+    }
+}
+
+impl InternedPathBuf {
+    pub fn from_iter<I, T>(iter: I) -> Self
+    where
+        I: IntoIterator<Item = T>,
+        T: Into<Interned>,
+    {
+        InternedPathBuf(
+            iter.into_iter()
+                .map(Into::into)
+                .map(Interned::into_inner)
+                .collect(),
+        )
+    }
+
+    pub fn from_str(s: &str) -> Self {
+        Self::from(s)
+    }
+
+    pub fn path(&self) -> &InternedPath {
+        &self.0
+    }
 }
