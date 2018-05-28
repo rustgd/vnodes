7d684c4889f9b62d402733d5ff6612aa7df67f47
torkleyy <torkleyy@gmail.com>
2018-05-28 08:44:08 +0200

    Restructure reexports and modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,7 @@
 #![allow(dead_code)]
 
-//#[macro_use]
-//extern crate bitflags;
-#[macro_use]
-extern crate derivative;
 #[macro_use]
 extern crate failure;
-extern crate fxhash;
 #[macro_use]
 extern crate log;
 extern crate parking_lot;
@@ -14,12 +9,19 @@ extern crate smallvec;
 
 mod macros;
 
+pub use any::Any;
+pub use context::Context;
+pub use intern::Interned;
+pub use node::{Node, NodePtr};
+pub use stack::{Error, Pop, Push, Result, Stack, StackTooSmall, Ty, UnexpectedTy};
+
 //pub mod raw;
 
+mod any;
+mod context;
 //mod conv;
 //mod data;
 //mod error;
-//mod fs;
 mod intern;
 //mod map;
 mod node;
