f9a0d5c583e3fc2924758c8236aa6eb3a81baa6f
torkleyy <torkleyy@gmail.com>
2018-05-28 08:45:11 +0200

    Use Arc for node pointers

--- src/node.rs
+++ src/node.rs
@@ -1,46 +1,36 @@
+use std::sync::Arc;
+
+use any::Any;
 use stack::Stack;
 
-#[repr(C)]
-pub struct NodeRaw {
-    pub raw: [u8; 32],
-}
-
-pub type NodeData = [u8; 24];
-
-#[repr(packed)]
-pub struct NodeFields {
-    data: NodeData,
-    vtable: *mut (),
-}
-
-pub trait NodeInterface {
+pub trait Node: Any {
     fn clone(&self, stack: &mut Stack);
     fn call(&self, stack: &mut Stack);
     fn get(&self, stack: &mut Stack);
     fn create(&self, stack: &mut Stack);
     fn get_or_create(&self, stack: &mut Stack);
     fn set(&self, stack: &mut Stack);
-    fn drop(&self, stack: &mut Stack);
 }
 
 #[repr(C)]
-pub struct NodeInterfaceRaw {
-    pub clone: extern "C" fn(*const NodeRaw, stack: &mut Stack),
-    pub call: extern "C" fn(*const NodeRaw, stack: &mut Stack),
-    pub get: extern "C" fn(*const NodeRaw, stack: &mut Stack),
-    pub create: extern "C" fn(*const NodeRaw, stack: &mut Stack),
-    pub get_or_create: extern "C" fn(*const NodeRaw, stack: &mut Stack),
-    pub set: extern "C" fn(*const NodeRaw, stack: &mut Stack),
-    pub drop: extern "C" fn(*const NodeRaw),
+pub struct NodePtr {
+    node: *const Node,
+}
+
+impl NodePtr {
+    pub fn from_arc(node: Arc<Node>) -> Self {
+        NodePtr {
+            node: Arc::into_raw(node),
+        }
+    }
+
+    pub fn into_arc(self) -> Arc<Node> {
+        unsafe { Arc::from_raw(self.node) }
+    }
 }
 
 mod tests {
-    use super::*;
-    use std::mem::size_of;
-
-    #[test]
-    fn check_correct_size() {
-        assert_eq!(size_of::<NodeFields>(), size_of::<Node>());
-        assert_eq!(size_of::<Node>(), 32);
-    }
+    //use super::*;
+    //use std::mem::size_of;
+    //use std::mem::transmute;
 }
